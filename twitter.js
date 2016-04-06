677bf47414def25be01e475c6ae9660a9396a298
sikB <sikhovski@icloud.com>
2016-04-06 17:09:19 -0400

    clean up

--- twitter.js
+++ twitter.js
@@ -16,12 +16,24 @@ tweetApp.controller('mainController', function($scope, $http, $routeParams, $int
 					var currentDate = new Date();
 					var currentTimeInSec = currentDate.getTime()/1000;
 					$scope.data[i].sinceTweeted = Math.floor(currentTimeInSec - $scope.data[i].tweetSeconds);
-
-				}
+					// this should be a function
+					var timeInSec = $scope.data[i].sinceTweeted;
+					if(timeInSec < 0){
+						timeInSec = 0;
+					}if(timeInSec  < 60){
+						timeInSec + 's ago';
+					}else if(timeInSec < 10 * 60){
+						var m = Math.floor(timeInSec / 60),
+						s = timeInSec - m * 60;
+						 m + 'm' + (s === 0 ? '' : ' ' + s + 's') + 'ago';
+					}else if (timeInSec < 60 * 60){
+						 (Math.floor(timeInSec/60)) + 'm ago';
+					}else if(timeInSec < 60 * 60 * 24){
+						(Math>floor(timeInSec/60/60)) + 'h ago';
+					} (Math.floor(timeInSec/60/60/24)) + 'd ago';
+				}   
+				// console.log(timeInSec);
 			},1000);
-
 		}
-
 	})
-
 });
